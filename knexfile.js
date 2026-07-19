efe81b424fe069fd1bf6d3189f1a9d406539c136
lito00356 <tomasz.liksza@gmail.com>
2026-07-19 18:16:37 +0200

    updated knex file

--- knexfile.js
+++ knexfile.js
@@ -3,23 +3,26 @@ dotenv.config();
 
 const dbName = process.env.DATABASE_NAME || "database.sqlite3";
 
-const config = {
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: `./${dbName}`, // the file that will store the database
-    },
-    useNullAsDefault: true,
-    migrations: {
-      tableName: "knex_migrations", // table that will store the migration history
-      directory: "./src/migrations", // location of the migration files
-      stub: "./migration.stub", // this is the file that will be copied when creating a new migration
-    },
-    seeds: {
-      directory: "./src/seeds", // location of the seed files (seeds = initial data for the database)
-      stub: "./seed.stub", // this is the file that will be copied when creating a new seed
-    },
+const baseConfig = {
+  client: "sqlite3",
+  connection: {
+    filename: `./${dbName}`,
   },
+  useNullAsDefault: true,
+  migrations: {
+    tableName: "knex_migrations",
+    directory: "./src/migrations",
+    stub: "./migration.stub",
+  },
+  seeds: {
+    directory: "./src/seeds",
+    stub: "./seed.stub",
+  },
+};
+
+const config = {
+  development: baseConfig,
+  production: baseConfig,
 };
 
 export default config;
